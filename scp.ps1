c200f956caf9908f0b7de12552972f5a4fdf2c7e
Yordii-dev <yokdy3602@gmail.com>
2026-02-09 15:24:25 -0500

    ffix: scp

--- scp.ps1
+++ scp.ps1
@@ -1,2 +1,2 @@
 scp .\.env .\.env.development .\.env.production ubuntu@3.22.200.156:/home/ubuntu/micro_api_wsp/
-#scp ubuntu@3.22.200.156:/home/ubuntu/micro_api_wsp/.env.* .
+#scp ubuntu@3.22.200.156:/home/ubuntu/micro_api_wsp/.env* .

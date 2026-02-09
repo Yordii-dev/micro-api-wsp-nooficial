cfdb81896a66ac3c65258b2349c3ad42c623e446
Yordii-dev <yokdy3602@gmail.com>
2026-02-09 15:18:30 -0500

    scp update get env from vps

--- scp.ps1
+++ scp.ps1
@@ -1,1 +1,2 @@
-scp .\.env.production ubuntu@3.22.200.156:/home/ubuntu/micro_api_wsp/
+scp .\.env .\.env.development .\.env.production ubuntu@3.22.200.156:/home/ubuntu/micro_api_wsp/
+#scp ubuntu@3.22.200.156:/home/ubuntu/micro_api_wsp/.env.* .
